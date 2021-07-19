301686eaac184ae53a734670011b9cf81345f194
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2021-07-19 10:58:46 +0600

    Design part of to do list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  todoList = [
+    {
+      itemName: 'Coding',
+      itemDueDate: '19-07-2021',
+      itemPriority: 'high',
+      itemCategory: 'work'
+    },
+    {
+      itemName: 'Designing',
+      itemDueDate: '19-07-2021',
+      itemPriority: 'low',
+      itemCategory: 'work'
+    },
+    {
+      itemName: 'shopping',
+      itemDueDate: '19-07-2021',
+      itemPriority: 'middle',
+      itemCategory: 'personal'
+    },
+    {
+      itemName: 'Dinner',
+      itemDueDate: '19-07-2021',
+      itemPriority: 'high',
+      itemCategory: 'personal'
+    }
+  ];
+
+  today: number = Date.now();
+
   constructor() {}
 
 }
